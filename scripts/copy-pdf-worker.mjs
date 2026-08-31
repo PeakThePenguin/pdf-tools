7a21aaaa9d580dfb868cd896226d601094cd2484
PeakThePenguin <peaksone@gmail.com>
2026-08-31 16:47:59 +0700

    Update copy-pdf-worker.mjs

--- scripts/copy-pdf-worker.mjs
+++ scripts/copy-pdf-worker.mjs
@@ -10,6 +10,7 @@ const src = path.join(__dirname, '../node_modules/pdfjs-dist/build/pdf.worker.mi
 const dest = path.join(__dirname, '../public/pdf.worker.min.mjs')
 
 if (fs.existsSync(src)) {
+  fs.mkdirSync(path.dirname(dest), { recursive: true })
   fs.copyFileSync(src, dest)
   console.log('Copied pdf.worker.min.mjs to public/')
 } else {
